af95b65052e193cdbe8a02810e431244613d474d
Silurana <silurana330@gmail.com>
2026-07-19 23:03:00 +0530

    feat: initialize typescript with nodejs

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import routes from './routes/index.js';
+
+dotenv.config();
+
+const app = express();
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/typeNode', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the API. Please use the appropriate endpoints to interact with the application.");
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
